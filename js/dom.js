31568661d8d7cbfba74a7a8ac62840d22500c68b
GritiusX <guidopawluk@gmail.com>
2021-11-09 14:38:22 -0300

    pusheando todo

---
+++ js/dom.js
@@ -0,0 +1,20 @@
+const agregarBtn = document.getElementById('agregarBtn');
+const editarBtn = document.getElementById('editarBtn');
+const borrarBtn = document.getElementById('borrarBtn');
+
+let tareas = [];
+let tareaInput = document.getElementById('tareaInput').value;
+
+console.log(agregarBtn);
+console.log(editarBtn);
+console.log(borrarBtn);
+console.log(tareaInput);
+console.log(tareas);
+tareaLink
+
+tareasSolicitadasContainer
+mesCambia
+tablaNumDias
+tablaCompletaMes
+tareaInput
+horarioInput
